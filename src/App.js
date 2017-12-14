a171c17033ee5bcfdddd881c4e441cbadea7b83b
Mary Chelle Carreon <marychellecarreon@gmail.com>
2017-12-14 19:22:29 +0700

    modified fetch api

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     super(props);
     this.state = {
       apiData: {},
-      data: []
+      variants: []
     }
   }
 
@@ -20,7 +20,7 @@ class App extends Component {
       this.setState(
         { apiData: apiJSON }, () => {
           this.setState(
-            { data: this.state.apiData.data }
+            { variants: this.state.apiData.variants }
           )
         }
       )
@@ -29,17 +29,10 @@ class App extends Component {
 
   render() {
     console.log(this.state.apiData)
-    let data_list = this.state.data.map((datum, index) => {
+    let data_list = this.state.variants.map((datum, index) => {
       return (
         <tr key={index}>
-        <td>{datum.description}</td>
-        <td>{datum.important_information}</td>
-        <td>{datum.tour_duration}</td>
-        <td>{datum.available_day}</td>
-        <td>{datum.meeting_time}</td>
-        <td>{datum.meeting_point}</td>
-        <td>{datum.variants.starts_on}</td>
-        <td>{datum.variants.ends_on}</td>
+        <td>{datum.starts_on}</td>
         </tr>
       )
     });
@@ -52,14 +45,7 @@ class App extends Component {
       </header>
       <thead>
       <tr>
-        <th>Description</th>
-        <th>Additional Information</th>
-        <th>Duration</th>
-        <th>Available Day</th>
-        <th>Meeting Time</th>
-        <th>Meeting Point</th>
-        <th>Start Date</th>
-        <th>End Date</th>
+        <th>Select Date</th>
       </tr>
         {data_list}
         </thead>
